940f54ac9fb50b5bca05f55555c7d1fe7bae55ed
bhole121 <bholeshwarj@gmail.com>
2023-10-18 10:22:37 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import uvicorn
+from app import models
+from app.db import engine
+from app.routes import router
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+app.include_router(router)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
