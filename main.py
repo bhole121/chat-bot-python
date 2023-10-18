7e13c8ce1bb88953623c82a5c454732dfe7729ba
bhole121 <bholeshwarj@gmail.com>
2023-10-18 10:25:39 +0530

    change

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app.include_router(router)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World 2"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
